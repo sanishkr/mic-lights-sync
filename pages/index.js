697f4da945983924a9fa21c60efd28397b40cad1
Sanish <sanishkr@gmail.com>
2021-07-16 22:15:31 +0000

    :alembic: removing secret key and adding to env variables

--- pages/index.js
+++ pages/index.js
@@ -14,9 +14,10 @@ export default function Home({cookies}) {
   // console.log({cookies});
   const [pubnub, setPubnub] = useState();
   useEffect(() => {
+    console.log(process.env.NEXT_PUBLIC_publishKey);
     setPubnub(new PubNub({
-      publishKey: 'pub-c-e87aa73d-3d6a-406f-88fb-54ce6270d019',
-      subscribeKey: 'sub-c-6aea92d4-e3b3-11eb-a999-22f598fbfd18',
+      publishKey: process.env.NEXT_PUBLIC_publishKey,
+      subscribeKey: process.env.NEXT_PUBLIC_subscribeKey,
       uuid: getUuid(),
     }));
   }, [])
